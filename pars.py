694eb4a43ad6b49b1c58be941ebf06639f2017b6
TUT.GURU <60303778+white-hackers@users.noreply.github.com>
2022-07-23 17:42:48 +0300

    Переписан весь код
    Все переписано для успешной работы

--- pars.py
+++ pars.py
@@ -19,9 +19,9 @@ def banner():
 ░░░██║░░░██║░░░██║░░░██║░░░░░░██║░░╚██╗██║░░░██║██╔══██╗██║░░░██║
 ░░░██║░░░╚██████╔╝░░░██║░░░██╗╚██████╔╝╚██████╔╝██║░░██║╚██████╔╝
 ░░░╚═╝░░░░╚═════╝░░░░╚═╝░░░╚═╝░╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░
-TUT.GURU by https://tut.guru/
+by https://tut.guru/
+
         """)
-        
 
 cpass = configparser.RawConfigParser()
 cpass.read('config.data')
@@ -42,7 +42,7 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     os.system('clear')
     banner()
-    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+    client.sign_in(phone, input(gr+'[+] Введите код: '+re))
  
 os.system('clear')
 banner()
@@ -67,22 +67,22 @@ for chat in chats:
     except:
         continue
  
-print(gr+'[+] Choose a group to scrape members :'+re)
+print(gr+'[+] Выберите группу для парсинга участников :'+re)
 i=0
 for g in groups:
     print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ g.title)
     i+=1
  
 print('')
-g_index = input(gr+"[+] Enter a Number : "+re)
+g_index = input(gr+"[+] Введите номер: "+re)
 target_group=groups[int(g_index)]
  
-print(gr+'[+] Fetching Members...')
+print(gr+'[+] Выборка файл для парсинга...')
 time.sleep(1)
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
  
-print(gr+'[+] Saving In file...')
+print(gr+'[+] Сохранено в файле...')
 time.sleep(1)
 with open("members.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
@@ -102,4 +102,4 @@ with open("members.csv","w",encoding='UTF-8') as f:
             last_name= ""
         name= (first_name + ' ' + last_name).strip()
         writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print(gr+'[+] Members scraped successfully.')
+print(gr+'[+] Участники успешно спаршены.')
